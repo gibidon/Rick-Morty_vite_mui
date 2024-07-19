741e2051dd3b97507d77c6e42146caa0b4bb0f21
gibidon <malykhinig@gmail.com>
2024-07-19 10:53:00 +0700

    chore: added simple loader to ItemTemplate

--- src/pages/DetailedPage/DetailedPage.tsx
+++ src/pages/DetailedPage/DetailedPage.tsx
@@ -6,7 +6,15 @@ import { ItemTemplate } from '../../components/ItemTemplate/ItemTemplate'
 
 export const DetailedPage = () => {
   const { category, id } = useParams<{ category: TCategoryName; id: string }>()
-  const { data } = useFetch<AllCategoryType>(`${BASE_URL}/${category}/${id}`)
+  const { data, isLoading, error } = useFetch<AllCategoryType>(
+    `${BASE_URL}/${category}/${id}`,
+  )
 
-  return <>{data && <ItemTemplate item={data} category={category} />}</>
+  return (
+    <>
+      {data && <ItemTemplate item={data} category={category} />}
+      {isLoading && <div>Loading..</div>}
+      {error && <div>Error: {error}</div>}
+    </>
+  )
 }
